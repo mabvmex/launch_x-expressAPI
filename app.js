96468af0bddf719202af6a8565635fee060d5be1
mabvmex <mabvmex@gmail.com>
2022-05-03 11:13:00 -0500

    GET: Endpoint con lista de explorers

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
   res.send('hola mundo')
 })
+
+app.get('/v1/explorers', (req, res) => {
+  console.log(`API Explorers: GET ALL request ${new Date()}`);
+  const explorer1 = {id: 1, name: "mabvmex" }
+  const explorer2 = {id: 2, name: "mabvmet" }
+  const explorer3 = {id: 3, name: "mabvdev" }
+  const explorer4 = {id: 4, name: "mabvskom" }
+  const explorer5 = {id: 5, name: "mabvmet" }
+
+  const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5, ];
+  res.status(200).json(explorers);
+})
