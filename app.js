6d9e65433e5ef7c2ca4480e0b0174dc471f373a6
mabvmex <mabvmex@gmail.com>
2022-05-03 11:47:18 -0500

    Enpoint GET by Id

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -6,20 +6,33 @@ app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Listening port on localhost:${port}`);
-})
+});
 
-app.get('/', (req, res) => {
-  res.send('hola mundo')
-})
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
 
-app.get('/v1/explorers', (req, res) => {
+app.get("/v1/explorers", (req, res) => {
   console.log(`API Explorers: GET ALL request ${new Date()}`);
-  const explorer1 = {id: 1, name: "mabvmex" }
-  const explorer2 = {id: 2, name: "mabvmet" }
-  const explorer3 = {id: 3, name: "mabvdev" }
-  const explorer4 = {id: 4, name: "mabvskom" }
-  const explorer5 = {id: 5, name: "mabvmet" }
+  const explorer1 = { id: 1, name: "mabvmex" };
+  const explorer2 = { id: 2, name: "mabvmet" };
+  const explorer3 = { id: 3, name: "mabvdev" };
+  const explorer4 = { id: 4, name: "mabvskom" };
+  const explorer5 = { id: 5, name: "mabvmet" };
 
-  const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5, ];
+  const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5];
   res.status(200).json(explorers);
+});
+
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`API Explorers: GET request ${new Date()}`);
+  console.log(`GET an explorer by Id ${req.params.id}`);
+  const explorer = { id: 1, name: "mabvmex" };
+  res.status(200).json(explorer);
+});
+
+app.post('/v1/explorers', (req, res) => {
+  console.log(`API Explorers: POST request ${new Date()}`);
+  const requestBody = req.body;
+  res.status(201).json({message: 'Created'})
 })
